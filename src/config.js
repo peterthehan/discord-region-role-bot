18f9a028286a65db6ab8ca85804520946b819900
Peter Han <peter.han.9493@gmail.com>
2019-05-08 00:20:13 -0400

    Change cmd limit to 1

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   token: ('TOKEN' in process.env ? process.env : require('../config')).TOKEN,
   prefix: '.',
   commandDelimiter: '\n',
-  commandLimit: '5',
+  commandLimit: '1',
   regionRoleLimit: 1
 };
