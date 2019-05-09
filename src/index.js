94a55d020ff987406627433ec867b2adcf6ae046
Peter Han <peter.han.9493@gmail.com>
2019-05-08 21:55:36 -0400

    Revert changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@
 
 const { Client } = require('discord.js');
 const { token } = require('./config');
-const requireUncached = require('./util/requireUncached');
 
 const client = new Client();
 
-requireUncached('./loadCommands')(client, __filename);
+require('./util/loadCommands')(client);
 require('./util/loadListeners')(client);
 
 client.login(token).catch(error => {
--- src/util/loadCommands.js
+++ src/util/loadCommands.js
@@ -1,11 +1,10 @@
 const fs = require('fs');
-const path = require('path');
 const { Collection } = require('discord.js');
 
-module.exports = (client, filename) => {
+module.exports = client => {
   const commands = new Collection();
 
-  fs.readdirSync(path.resolve(filename, '../commands'))
+  fs.readdirSync('./src/commands')
     .filter(file => file.endsWith('.js'))
     .forEach(file => {
       const command = require(`../commands/${file}`);
