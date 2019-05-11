4912471ca6604de6ac3ea007e4e3dd512d3ee9c3
Peter Han <peter.han.9493@gmail.com>
2019-05-11 01:15:58 -0400

    Test resolve

--- src/util/loadCommands.js
+++ src/util/loadCommands.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const { Collection } = require('discord.js');
 
 module.exports = client => {
@@ -7,7 +8,8 @@ module.exports = client => {
   console.log('dirname', __dirname);
   console.log('filename', __filename);
   console.log('process', process.cwd());
-  fs.readdirSync('./src/commands')
+  console.log('resolve', path.resolve(__dirname, '../commands'));
+  fs.readdirSync(path.resolve(__dirname, '../commands'))
     .filter(file => file.endsWith('.js'))
     .forEach(file => {
       const command = require(`../commands/${file}`);
