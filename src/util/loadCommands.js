7af9aa4d0387122c977cd69f1342ea6fa94015f3
Peter Han <peter.han.9493@gmail.com>
2019-05-11 01:08:41 -0400

    Test

--- src/util/loadCommands.js
+++ src/util/loadCommands.js
@@ -4,6 +4,9 @@ const { Collection } = require('discord.js');
 module.exports = client => {
   const commands = new Collection();
 
+  console.log('dirname', __dirname);
+  console.log('filename', __filename);
+  console.log('process', process.cwd());
   fs.readdirSync('./src/commands')
     .filter(file => file.endsWith('.js'))
     .forEach(file => {
